ae4ce8822f58b74f7c99417d7c4fe06386fc304a
ehsanghaffarii <ghafari.5000@gmail.com>
2023-12-19 07:19:33 +0330

    Update OpenAI API key variable name

--- lib/OpenAiCompletaions.ts
+++ lib/OpenAiCompletaions.ts
@@ -5,8 +5,10 @@ import { Configuration, OpenAIApi } from 'openai'
 // }
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
+
+console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY)
 
 const openai = new OpenAIApi(configuration);
 
