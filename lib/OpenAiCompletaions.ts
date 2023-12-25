2a339344a170753fd211127d305e888b45a58188
ehsanghaffarii <ghafari.5000@gmail.com>
2023-12-25 06:37:56 +0330

    Remove console.log statement

--- lib/OpenAiCompletaions.ts
+++ lib/OpenAiCompletaions.ts
@@ -8,8 +8,6 @@ const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
-console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY)
-
 const openai = new OpenAIApi(configuration);
 
 export default openai
