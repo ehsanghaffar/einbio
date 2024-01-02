b05b68dd625a0fb38be5865629d4087e79d4070c
ehsanghaffarii <ghafari.5000@gmail.com>
2024-01-02 19:20:36 +0330

    Add Header, Footer, and Textarea componentsThe changes include adding the Header and Footer components to the layout, as well as adding a new Textarea component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
+import Header from '@/components/Header';
+import './globals.css';
+import Footer from '@/components/Footer';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="fa">
-      <body>{children}</body>
+    <html lang="fa" dir="rtl">
+      <body className="flex mx-auto flex-col py-2 min-h-screen">
+        <Header />
+        <main className="flex flex-1 w-full flex-col items-center justify-center">
+        {children}
+        </main>
+        <Footer />
+        </body>
     </html>
   )
 }
