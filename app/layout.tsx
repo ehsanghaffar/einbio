6c6ee70939cb2f753d909d3ce88749339dc94251
ehsanghaffarii <ghafari.5000@gmail.com>
2024-05-05 06:04:33 +0330

    Add metadata and rename screenshot

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,32 @@ import Header from '@/components/Header';
 import './globals.css';
 import Footer from '@/components/Footer';
 import { Toaster } from '@/components/ui/sonner';
+import { Metadata } from 'next';
+
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+
+export const metadata: Metadata = {
+  title: 'بایو جی پی تی',
+  description: 'ساخت بایو حرفه ای با یک کلیک',
+  alternates: {
+    canonical: '/'
+  },
+  openGraph: {
+    title: 'بایو جی پی تی',
+    description: 'ساختن بایو با یک کلیک همراه چت جی پی تی',
+    images: [{
+      url: 'https://biogpt-chi.vercel.app/screenshot.png',
+      width: '300',
+      height: '300',
+      alt: "ساختن بایو با یک کلیک همراه چت جی پی تی"
+    }]
+  },
+}
+
 
 export default function RootLayout({
   children,
