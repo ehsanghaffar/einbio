547f000e4329e8bf822ad4c4e606d0fbd93cd3db
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-06-06 00:18:10 +0900

    Update route.ts
    requset -> request

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,8 +5,8 @@ import { errorHandler } from "@/libs/utils";
 
 const apikey = process.env.NEXT_PUBLIC_OPENAI_API_KEY
 
-export async function POST(requset: Request) {
-  const messages = await requset.json()
+export async function POST(request: Request) {
+  const messages = await request.json()
   if (!messages) {
     return new Response("No messages provided", { status: 400 })
   }
