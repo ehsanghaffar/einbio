2056700e32ba7793f8cfa6e5f179721c7cf8e33b
ehsanghaffarii <ghafari.5000@gmail.com>
2024-12-15 04:42:38 +0330

    Fix code formatting in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,57 @@
-import Header from '@/components/Header';
-import '@/styles/globals.css';
-import Footer from '@/components/Footer';
-import { Toaster } from '@/components/ui/sonner';
-import { Metadata } from 'next';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
+import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export const metadata: Metadata = {
-  title: 'در کسری از ثانیه، بایویی خیره‌کننده و جذاب بساز!',
-  description: 'اینجا می‌تونی با چند کلیک ساده، بایویی شخصی‌سازی شده و جذاب که نمایانگر واقعی شخصیت توه، بسازی.',
+  title: "در کسری از ثانیه، بایویی خیره‌کننده و جذاب بساز!",
+  description:
+    "اینجا می‌تونی با چند کلیک ساده، بایویی شخصی‌سازی شده و جذاب که نمایانگر واقعی شخصیت توه، بسازی.",
   alternates: {
-    canonical: '/'
+    canonical: "/",
   },
   openGraph: {
-    title: 'در کسری از ثانیه، بایویی خیره‌کننده و جذاب بساز!',
-    description: 'اینجا می‌تونی با چند کلیک ساده، بایویی شخصی‌سازی شده و جذاب که نمایانگر واقعی شخصیت توه، بسازی.',
-    images: [{
-      url: 'https://bio.eindev.ir/screenshot.png',
-      width: '300',
-      height: '300',
-      alt: "با استفاده از جدیدترین فناوری هوش مصنوعی، ما به شما کمک می‌کنیم تا بایویی ایجاد کنید که همه را مجذوب خود کند."
-    }]
+    title: "در کسری از ثانیه، بایویی خیره‌کننده و جذاب بساز!",
+    description:
+      "اینجا می‌تونی با چند کلیک ساده، بایویی شخصی‌سازی شده و جذاب که نمایانگر واقعی شخصیت توه، بسازی.",
+    images: [
+      {
+        url: "https://bio.eindev.ir/screenshot.png",
+        width: "300",
+        height: "300",
+        alt: "با استفاده از جدیدترین فناوری هوش مصنوعی، ما به شما کمک می‌کنیم تا بایویی ایجاد کنید که همه را مجذوب خود کند.",
+      },
+    ],
   },
-}
-
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fa" dir="rtl">
       <body className="min-h-screen flex flex-col">
         <header>
-        <Header />
+          <Header />
         </header>
-        <main className="flex flex-1">
-        {children}
-        </main>
-        <footer className='flex flex-col w-full bottom-0'>
-        <Footer />
+        <main className="flex flex-1">{children}</main>
+        <footer className="flex flex-col w-full bottom-0">
+          <Footer />
         </footer>
         <Toaster richColors />
         <Analytics />
         <SpeedInsights />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
